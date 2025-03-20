dd82d49fbacba5782c02ec3a398c94b32efc23fe
rmncardoso <ruicard@amazon.pt>
2025-03-20 21:55:49 +0000

    update deploy cloud

--- scripts/deploy-cloud.sh
+++ scripts/deploy-cloud.sh
@@ -4,6 +4,22 @@ set -e
 sudo yum -y update
 sudo yum -y install jq gettext bash-completion
 
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+nvm current # Should print "v22.14.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
 # Set up AWS environment
 if [ -z "${ACCOUNT_ID}" ]; then
     ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
